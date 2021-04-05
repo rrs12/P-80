6b12e0aa659d21e3060df94cb8b267f4da5f159b
rrs12 <77575080+rrs12@users.noreply.github.com>
2021-04-05 20:13:52 +0530

    Update main.js

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ function para1() {
         inputs.push(document.getElementById("p1_ib_"+i).value);
        console.log(inputs);
     }
-    document.getElementById("showpara1").innerHTML = inputs.join(". ")+(".");
+    document.getElementById("showpara1").innerHTML = inputs.join(". ")+".";
 }
 
 function getpara2() {
@@ -12,7 +12,7 @@ function getpara2() {
     for (var i = 1; i <= 6; i++) {
         inputs.push(document.getElementById("p2_ib_" + i).value);
     }
-    document.getElementById("showpara2").innerHTML = inputs.join(". ")+(".");
+    document.getElementById("showpara2").innerHTML = inputs.join(". ")+".";
 }
 
 function getpara3(){
